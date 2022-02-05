ca498ee92a59a61ada22a0a6588cbb36dc7afba5
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-04 23:44:09 -0700

    fixed errors prod build

--- components/navigator/index.js
+++ components/navigator/index.js
@@ -20,26 +20,26 @@ export default function Navigator() {
                   <span className="sr-only">Open main menu</span>
 
                   <svg onClick={handleShow} className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                   </svg>
 
                   <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                   </svg>
                 </button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <a as={Link} href="/" ><img className="block lg:hidden h-8 w-auto hover:bg-opacity-30 hover:cursor-pointer" src="/s-logo.png" alt="Workflow" /></a> 
-                  <a as={Link} href="/"><img className="hidden lg:block h-8 w-auto hover:bg-opacity-30 hover:cursor-pointer" src="/s-logo.png" alt="Workflow" /></a>
+                  <Link href="/" ><a><img className="block lg:hidden h-8 w-auto hover:bg-opacity-30 hover:cursor-pointer" src="/s-logo.png" alt="Workflow" /></a></Link> 
+                  <Link href="/"><a><img className="hidden lg:block h-8 w-auto hover:bg-opacity-30 hover:cursor-pointer" src="/s-logo.png" alt="Workflow" /></a></Link>
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4 absolute right-0">
-                    <a href="/sounds" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Sounds</a>
+                    <Link href="/sounds"><a className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Sounds</a></Link>
 
-                    <a href="/sign-in" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
+                    <Link href="/sign-in"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a></Link>
 
-                    <a href="/sign-up" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign Up</a>
+                    <Link  href="/sign-up"><a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign Up</a></Link>
                   </div>
                 </div>
               </div>
@@ -68,11 +68,11 @@ export default function Navigator() {
           id="mobile-menu"
           style={{ display: show ? "block" : "none" }}>
             <div className="px-2 pt-2 pb-3 space-y-1">
-              <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Sounds</a>
+              <Link href="/sounds" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Sounds</Link>
 
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Login</a>
+              <Link href="/sign-in" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Login</Link>
 
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign Up</a>
+              <Link href="/sign-up" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign Up</Link>
             </div>
           </div>
         </nav>
--- pages/sign-up/index.js
+++ pages/sign-up/index.js
@@ -1,4 +1,4 @@
-
+import Link from 'next/link'
 
 export default function SignIn() {
 
@@ -15,20 +15,20 @@ export default function SignIn() {
                 <input type="hidden" name="remember" value="true" />
                 <div className="rounded-lg shadow-sm -space-y-px">
                     <div className="py-2">
-                    <label for="email-address" className="sr-only">Email address</label>
-                    <input id="email-address" name="email" type="email" autocomplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+                    <label htmlFor="email-address" className="sr-only">Email address</label>
+                    <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
                     </div>
                     <div className="py-2">
-                    <label for="password" className="sr-only">Password</label>
-                    <input id="password" name="password" type="password" autocomplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+                    <label htmlFor="password" className="sr-only">Password</label>
+                    <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
                     </div>
                     <div className="py-2">
-                    <label for="firstName" className="sr-only">First Name</label>
-                    <input id="first-name" name="first-name" type="first-name" autocomplete="current-first-name" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="First Name" />
+                    <label htmlFor="firstName" className="sr-only">First Name</label>
+                    <input id="first-name" name="first-name" type="first-name" autoComplete="current-first-name" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="First Name" />
                     </div>
                     <div className="py-2">
-                    <label for="lastName" className="sr-only">Last Name</label>
-                    <input id="password" name="password" type="" autocomplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Last Name" />
+                    <label htmlFor="lastName" className="sr-only">Last Name</label>
+                    <input id="password" name="password" type="" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Last Name" />
                     </div>
                 </div>
 
@@ -37,7 +37,7 @@ export default function SignIn() {
                     <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                         <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                         </svg>
                     </span>
                     Sign Up
@@ -45,7 +45,7 @@ export default function SignIn() {
                 </div>
                 </form>
                 <div className="text-sm text-center">
-                    Already have an account? <a href="/sign-in" className="font-medium text-indigo-600 hover:text-indigo-500"> Sign In </a>
+                    Already have an account? <Link href="/sign-in"><a className="font-medium text-indigo-600 hover:text-indigo-500">Sign In</a></Link>
                 </div>
             </div>
             </div>
