c514c730d26e3db9f3a36a1b5ddf5dc480364de2
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-16 20:31:49 -0700

    added image and started landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main': "url('https://creativetechlab.com/wp-content/uploads/2016/08/music-producer-disconnected-1280x640.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   variants: {},
   plugins: [],
