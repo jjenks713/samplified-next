e8c82e3f6136b1734c330456c25c0fb207038248
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-02-27 11:31:10 -0700

    testing form

--- pages/upload/index.js
+++ pages/upload/index.js
@@ -84,7 +84,7 @@ export default function Upload() {
                 <p className="mt-2 text-center text-sm text-gray-600">
                 </p>
             </div><br></br><br></br>
-            <form onSubmit={submitForm} className="w-full max-w-sm">
+            <form className="w-full max-w-sm">
                 <div className="md:flex md:items-center mb-6">
                     <div className="md:w-1/3">
                     <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
@@ -214,7 +214,7 @@ export default function Upload() {
                 <div className="md:flex md:items-center">
                     <div className="md:w-1/3"></div>
                     <div className="md:w-2/3">
-                    <button type="submit" className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                    <button onClick={submitForm} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
                         Submit
                     </button>
 
