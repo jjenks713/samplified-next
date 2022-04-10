ec5222444aa08e58dab97089a8b134a76e453edf
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-04-10 10:08:45 -0600

    bg on profile form

--- pages/userpage/[[...id]].js
+++ pages/userpage/[[...id]].js
@@ -20,7 +20,7 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: 'background.paper',
+  bgcolor: '#151B23',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
